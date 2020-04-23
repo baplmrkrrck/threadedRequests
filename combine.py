5a27485f1af6582dfbe4d9d5fd30e9aa7d2b1705
baplmrkrrck <31512565+baplmrkrrck@users.noreply.github.com>
2020-04-23 17:59:40 -0400

    Fixed

--- combine.py
+++ combine.py
@@ -1,4 +1,4 @@
-import csv, threading, json
+import csv, threading, json, os
 
 
 table_headers = ["prod_id","prod_sku","prod_cat","prod_name"]
@@ -7,8 +7,14 @@ writer = csv.DictWriter(f, fieldnames=table_headers)
 writer.writerow({"prod_id": "prod_id","prod_sku": "prod_sku","prod_cat": "prod_cat","prod_name": "prod_name"})
 
 temp = []
-reader = open("json.txt", "r")
-for line in reader:
-    if "prod_id" in line:
-        l = json.loads(line)["data"]
-        writer.writerow({"prod_id": l["prod_id"],"prod_sku": l["prod_sku"],"prod_cat": l["prod_cat"],"prod_name": l["prod_name"]})
+for root,dirs, files in os.walk('data'):
+    #print(root,dirs,files)
+    path = root.split(os.sep)
+    for fn in files:
+        fp = root+os.sep+fn
+
+        f = open(fp, 'r')
+        print(fp)
+        for line in f:
+            l = json.loads(line)["data"]
+            writer.writerow({"prod_id": l["prod_id"],"prod_sku": l["prod_sku"],"prod_cat": l["prod_cat"],"prod_name": l["prod_name"]})
--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-import requests, json, threading
-
-file = open("json.txt", "w")
-
+import requests, json, threading, os
+path = "data/"
 def worker(n):
     r = requests.get("https://clarksonmsda.org/api/get_product.php?pid=" + str(n))
 
-    if "pid" in r.text:
+    if "prod_id" in r.text:
+
+        file = open(path + str(n) + ".txt", "w")
         file.write(r.text + "\n")
+        file.close
         print(str(n))
 
+
+for root,dirs, files in os.walk(path):
+    for fn in files:
+        fp = root+os.sep+fn
+        os.remove(fp)
+
+if os.path.isdir(path) == False:
+    os.makedirs(path)
 i = 0
 
 while i <= 200:
